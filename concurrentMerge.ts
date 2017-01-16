d7347fc6b2b550c45347b32a24687401302097b8
Morten Anton Bach Sjøgren <msn@nota.dk>
2017-01-16 17:03:37 +0100

    Initial commit

---
+++ concurrentMerge.ts
@@ -0,0 +1,33 @@
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/defer'
+import 'rxjs/add/observable/of'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/mergeAll'
+
+function concurrentMerge<T>(this: Observable<T>, cb: (val: T) => Observable<T> | Promise<T>, concurrent?: number): Observable<T> {
+  return Observable.create((observer) => {
+    const worker = this.map((val: T, index: number) => {
+      return Observable.defer(() => cb(val)).map((res) => ({index, res}));
+    })
+    .mergeAll(concurrent);
+
+    const output: T[] = [];
+
+    worker.subscribe(
+      ({index, res}) => {
+        output[index] = res;
+      },
+      (err) => Observable.throw(err),
+      () => {
+        Observable.of(output).subscribe(observer);
+      });
+  });
+}
+
+Observable.prototype.concurrentMerge = concurrentMerge;
+
+declare module 'rxjs/Observable' {
+  interface Observable<T> {
+    concurrentMerge: typeof concurrentMerge;
+  }
+}
