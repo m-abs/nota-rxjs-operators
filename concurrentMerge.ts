d950314e62f25cfe85c7526dcda7496a6cce196b
Morten Anton Bach Sjøgren <msn@nota.dk>
2017-03-28 14:50:02 +0200

    fix: typings

--- concurrentMerge.ts
+++ concurrentMerge.ts
@@ -4,7 +4,7 @@ import 'rxjs/add/observable/of'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/mergeAll'
 
-export function concurrentMerge<T>(this: Observable<T>, cb: (val: T) => Observable<T> | Promise<T>, concurrent?: number): Observable<T> {
+export function concurrentMerge<T>(this: Observable<any>, cb: (val: any) => Observable<T> | Promise<T>, concurrent?: number): Observable<T[]> {
   return Observable.create((observer) => {
     const worker = this.map((val: T, index: number) => {
       return Observable.defer(() => cb(val)).map((res) => ({index, res}));
