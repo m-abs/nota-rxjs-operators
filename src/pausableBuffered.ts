171f22c75fd7eda3c9b52cd18790167234ea00d3
Morten Anton Bach Sjøgren <msn@nota.dk>
2017-12-07 12:00:26 +0100

    new pauseableBuffered

--- src/pausableBuffered.ts
+++ src/pausableBuffered.ts
@@ -1,24 +1,56 @@
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/buffer';
+import 'rxjs/add/operator/bufferCount';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/observable/from';
 
 /**
  * Buffer the last {bufferCount} values while the pauser is true and emit them once it becomes false.
  */
-export function pausableBuffered<T>(this: Observable<T>, pauser: Observable<boolean>, bufferCount?: number): Observable<T> {
-  const buffer = new Subject<T[]>();
+export function pausableBuffered<T>(this: Observable<T>, pauser: Observable<boolean>, bufferSize: number = 1): Observable<T> {
+  return new Observable((subscriber) => {
+    let isEnabled = false;
+    const closeBuffer = new Subject();
+    const bufferIn = new Subject<T>();
 
-  let tmp = this.buffer(pauser);
+    const bufferSubscript = bufferIn
+      .buffer(closeBuffer)
+      .bufferCount(bufferSize)
+      .switchMap((values) => Observable.from(values))
+      .subscribe((values) => {
+        values.forEach((val) => subscriber.next(val));
+      });
 
-  if (bufferCount !== void 0) {
-    tmp = tmp.take(bufferCount);
-  }
+    const pauserSubcription = pauser.subscribe((pause) => {
+      isEnabled = pause;
+      if (isEnabled) {
+        // flush buffer every when stream is enabled
+        closeBuffer.next(0);
+      }
+    });
 
-  tmp.subscribe(buffer);
+    const subscription = this.subscribe((value) => {
+        try {
+          if (isEnabled) {
+            subscriber.next(value);
+          } else {
+            bufferIn.next(value);
+          }
+        } catch(err) {
+          subscriber.error(err);
+        }
+      },
+      (err) => subscriber.error(err),
+      () => subscriber.complete());
 
-  return pauser.switchMap((paused) => paused ? buffer : this);
+    return () => {
+      subscription.unsubscribe();
+      bufferSubscript.unsubscribe();
+      pauserSubcription.unsubscribe();
+    };
+  });
 }
 
 declare module 'rxjs/Observable' {
